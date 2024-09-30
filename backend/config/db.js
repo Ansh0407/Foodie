03dcf026f8143670f0da85cf0993c6fb21f8963b
ansh0407 <anshbhandari.13@gmail.com>
2024-09-30 09:51:53 +0530

    init certificate

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2');
-const fs = require('fs');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -9,14 +8,13 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,          // Make sure the port is included in your .env file
+    port: process.env.DB_PORT,
     ssl: {
-        ca: fs.readFileSync(process.env.DB_SSL_CERT_PATH)  // Path to the Aiven CA certificate
-    },
-    connectTimeout: 10000  // 10 seconds timeout (you can adjust this if needed)
+        rejectUnauthorized: false,
+        ca: process.env.DB_SSL_CERT_PATH
+    }
 });
 
-// Connect to the database
 db.connect((err) => {
     if (err) {
         console.error('Database connection error:', err);
